ef5b5660dac1e2bba9fd9ec53f5a64829d4b7de2
justMark <aropon.markjames.august172gmail.com>
2024-11-30 11:41:46 +0800

    accept current changes

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -33,17 +33,10 @@ class _DashboardState extends State<Dashboard> {
           const Dashboardlp(),
           const Tracker(),
           SizedBox(
-<<<<<<< HEAD
             child: Image.asset('assets/icons/group127.png'),
           ),
           SizedBox(
             child: Image.asset('assets/icons/group152.png'),
-=======
-            child: Image.asset('assets/group127.png'),
-          ),
-          SizedBox(
-            child: Image.asset('assets/group152.png'),
->>>>>>> 3f87042ab3e85ba53bb8da5bb8e17ecb2880fed1
           ),
         ],
       ),
@@ -52,7 +45,6 @@ class _DashboardState extends State<Dashboard> {
         onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
-<<<<<<< HEAD
             icon:
                 Image.asset('assets/icons/Group127.png', width: 20, height: 20),
             label: 'HomePage',
@@ -65,17 +57,6 @@ class _DashboardState extends State<Dashboard> {
           BottomNavigationBarItem(
             icon: Image.asset('assets/icons/account_box.png',
                 width: 20, height: 20),
-=======
-            icon: Image.asset('assets/Group127.png', width: 20, height: 20),
-            label: 'HomePage',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/Group152.png', width: 20, height: 20),
-            label: 'Monitoring',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/account_box.png', width: 20, height: 20),
->>>>>>> 3f87042ab3e85ba53bb8da5bb8e17ecb2880fed1
             label: 'Profile',
           ),
         ],
